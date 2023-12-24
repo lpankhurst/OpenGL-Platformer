70811634826ca89c78dfdb7721b55981dcaa5b74
n40643lp <lewis.pankhurst@student.manchester.ac.uk>
2023-12-24 03:02:35 +0000

    Basic window/gameloop

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]){
+
+    // Create the window
+    SDL_Window* window = SDL_CreateWindow("Hello", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
+    if ( window == nullptr )
+        std::cout << "Error Creating Window: " << SDL_GetError() << std::endl;
+
+    // Create the renderer onto the window
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if ( renderer == NULL )
+        std::cout << "Error Creating Renderer: " << SDL_GetError() << std::endl; 
+
+    std::cout << "Ran" << std::endl;
+
+    // Basic gameloop
+    while ( true ){
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        if ( event.type == SDL_QUIT )
+            break;
+        
+        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+        SDL_RenderPresent(renderer);
+        SDL_RenderClear(renderer);
+        SDL_Delay(5);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+
+    return 0;
+}
